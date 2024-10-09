b2651444425a958a7be4851d28caa678ecbb5c55
Simcha85 <169007313+Simcha85@users.noreply.github.com>
2024-10-09 17:11:08 +1300

    Update Corolla.py

--- Corolla.py
+++ Corolla.py
@@ -3,13 +3,23 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 
-data=pd.read_excel('ToyotaCorolla.xls')
+data = pd.read_csv(r'c:\Users\scman\Downloads\ToyotaCorolla.csv')
 
 
 st.title('USED TOYOTA COROLLA PRICES')
 st.subheader('RAW DATA')
 st.write(data)
 
+models=data['Model'].unique
+
 st.image('Toyota.jpg')
 
-st.selectbox('Model')
+sales1=data[['Model','Mfg_Year','Price']]
+
+fig=px.scatter(sales1,x='Mfg_Year',y='Price',color='Model')
+st.write(fig)
+
+sales2=data[['Fuel_Type','Mfg_Year','Price']]
+
+fig2=px.scatter_3d(sales2,x='Mfg_Year',y='Price',z='Fuel_Type',color='Price')
+st.write(fig2)
